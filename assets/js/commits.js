1ee75984ed76f635386bc8e0d074854d02eceddd
i960107 <i960107@naver.com>
2022-03-31 23:14:05 +0900

    Add Algorithms

--- assets/js/commits.js
+++ assets/js/commits.js
@@ -1,27 +1,40 @@
 const  USER = 'i960107'
 const EMAIL = 'i960107@naver.com'
 
-fetch('https://api.github.com/users/i960107/events',{
+const response = fetch('https://api.github.com/users/i960107/events',{
     headers : {"Content-Type" : "application/json"}
     // body : {
         // since:,
         // until:,
     // }
-}).then(events => {
-    let lastCommit
-    events.some(event =>{
-        return event.type == 'PushEvent' && event.payload.commits.reverse().some(commit => {
-          if (commit.author.email == EMAIL) {
-              lastCommit = {
-                  message: commit.message
-              }
-              return true
-          } 
-          return false
-        })
-    })
-    console.log(lastCommit)
-    return lastCommit
 })
+// .then(events => {
+//     let lastCommit 
+//     console.log(typeof JSON.stringify(events))
+//    (events, event =>{
+//         return event.type == 'PushEvent'
+//     })
+//     events.some(event =>{
+//         // return event.type == 'PushEvent' && event.payload.commits.reverse().some(commit => {
+//         //   if (commit.author.email == EMAIL) {
+//         //       lastCommit = {
+//         //           message: commit.message
+//         //       }
+//         //       return true
+//         //   } 
+//         //   return false
+//         // })
+//         // console.log(event)
+//         return event.type == 'PushEvent'
+//     })
+//     console.log(lastCommit)
+//     return lastCommit
+// })
 
-console.log("안녕")
+.then(events=>{
+    return events.json()
+})
+.then(events=>{
+    console.log( events)
+})
+console.log(response)
